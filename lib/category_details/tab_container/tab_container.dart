f29f01e9e8ea7a1421a3631e3fab695364b68b68
Mohamed <ma.multipharma@gmail.com>
2023-10-24 21:22:05 +0300

    completed detailing page

--- lib/category_details/tab_container/tab_container.dart
+++ lib/category_details/tab_container/tab_container.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:provider/provider.dart';
 import 'package:route_news_project/category_details/tab_container/cubit/tab_container_view_model.dart';
 import 'package:route_news_project/category_details/tab_container/tab_item.dart';
+
 import '../../models/category.dart';
-import '../cubit/category_details_view_model.dart';
 import '../cubit/states.dart';
 import '../news_container/news_container.dart';
 
@@ -30,88 +29,45 @@ class _TabContainerState extends State<TabContainer> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder <TabContainerViewModel, SourceStates>(
+    return BlocBuilder<TabContainerViewModel, SourceStates>(
         bloc: viewModel,
         builder: (context, state) {
           if (state is SourceLoadingState) {
-            return Center(child: CircularProgressIndicator(color: Theme
-                .of(context)
-                .primaryColor,),);
+            return Center(
+              child: CircularProgressIndicator(
+                color: Theme.of(context).primaryColor,
+              ),
+            );
           } else if (state is SourceErrorState) {
-            return Column(children: [
-              Text(state.errorMessage),
-            ],);
-          } else {
+            return Column(
+              children: [
+                Text(state.errorMessage),
+              ],
+            );
+          } else if (state is SourceSuccessState) {
             return DefaultTabController(
-                length:,
-                child: Column(
-                    children: [
-                    TabBar(
-                    isScrollable: true,
-                    indicatorColor: Colors.transparent,
-                    onTap: (index) {
-                      selectedIndex = index;
-                      setState(() {});
-                    },
-                    tabs: state.
-                    .map((source) => TabItem(
-            isSelected: selectedIndex ==
-            viewModel.sourcesList?.indexOf(source),
-          source: source))
-              .toList()),
-          NewsContainer(
-          source: viewModel.sourcesList![selectedIndex
-          ]
-          ,
-          )
-          ]
-          );
-        }
-        }
-    );
+                length: state.sourceList.length,
+                child: Column(children: [
+                  TabBar(
+                      isScrollable: true,
+                      indicatorColor: Colors.transparent,
+                      onTap: (index) {
+                        selectedIndex = index;
+                        setState(() {});
+                      },
+                      tabs: state.sourceList
+                          .map((source) => TabItem(
+                              isSelected: selectedIndex ==
+                                  state.sourceList.indexOf(source),
+                              source: source))
+                          .toList()),
+                  NewsContainer(
+                    source: state.sourceList[selectedIndex],
+                  )
+                ]));
+          } else {
+            return Container();
+          }
+        });
   }
 }
-//ChangeNotifierProvider(
-//       create: (context) => viewModel,
-//       child:
-//           Consumer<TabContainerViewModel>(builder: (context, viewModel, child) {
-//         if (viewModel.errorMessage != null) {
-//           return Column(
-//             children: [
-//               Text(viewModel.errorMessage ?? ''),
-//               ElevatedButton(onPressed: () {}, child: const Text('Try Again'))
-//             ],
-//           );
-//         } else if (viewModel.sourcesList == null) {
-//           return Center(
-//             child: CircularProgressIndicator(
-//               color: Theme.of(context).primaryColor,
-//             ),
-//           );
-//         } else {
-//           return DefaultTabController(
-//             length: viewModel.sourcesList?.length ?? 0,
-//             child: Column(
-//               children: [
-//                 TabBar(
-//                     isScrollable: true,
-//                     indicatorColor: Colors.transparent,
-//                     onTap: (index) {
-//                       selectedIndex = index;
-//                       setState(() {});
-//                     },
-//                     tabs: viewModel.sourcesList!
-//                         .map((source) => TabItem(
-//                             isSelected: selectedIndex ==
-//                                 viewModel.sourcesList?.indexOf(source),
-//                             source: source))
-//                         .toList()),
-//                 NewsContainer(
-//                   source: viewModel.sourcesList![selectedIndex],
-//                 )
-//               ],
-//             ),
-//           );
-//         }
-//       }),
-//     );
